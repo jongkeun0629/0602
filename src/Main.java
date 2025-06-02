07870e26022863faf8a628189f07025bf7e048fb
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-02 15:11:21 +0900

    switch to if

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,14 @@ public class Main {
     public static void main(String[] args) {
         String mood = "happy";
 
-        switch (mood){
-            case "happy":
-                System.out.println("^^");
-                break;
-            case "sad":
-                System.out.println("ㅠㅠ");
-                break;
-            case "angry":
-                System.out.println("화가 난다");
-                break;
-            case "tired":
-                System.out.println("잠이 온다");
-                break;
-            default:
-                System.out.println("몰루");
+        if(mood == "happy"){
+            System.out.println("해삐");
+        } else if(mood == "angry"){
+            System.out.println("화가 난다");
+        } else if(mood == "sad") {
+            System.out.println("슬프다");
+        } else {
+            System.out.println("몰루");
         }
     }
 }
