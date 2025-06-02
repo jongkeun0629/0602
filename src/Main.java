13df8cf207d2870d76bc30769ee606edb76a23b8
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-02 11:24:29 +0900

    In/Decrease

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,11 @@ public class Main {
         int a = 10;
         int b = 3;
 
-        a += 5;
+        System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(a++);
+        System.out.println(b--);
         System.out.println(a);
-
-        b -= 1;
-        b *= 4;
-        b /= 2;
-        b %= 3;
         System.out.println(b);
     }
 }
