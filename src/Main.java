d8f2f5253dc0da3d7661f8ffda02b37e4039db83
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-02 10:09:09 +0900

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        final int DAYS_IN_WEEK = 7;
+        final double PI = 3.14159;
+        final String GREETING = "Hello, world!";
 
+        System.out.println("Days in a week: " + DAYS_IN_WEEK);
+        System.out.println("Value of PI: " + PI);
+        System.out.println("Greeting: " + GREETING);
+
+        // Error:
+        // DAYS_IN_WEEK = 8;
     }
 }
